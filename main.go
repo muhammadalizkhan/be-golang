01137b5f72e196d4cb3b97a63a36ec892a0eed1a
Muhamad Ali Khan <ali7847khan@gmail.com>
2025-01-18 21:59:30 +0500

    feat: Serve static files

--- main.go
+++ main.go
@@ -8,7 +8,15 @@ import (
 )
 
 func main() {
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	router := routes.RegisterRoutes()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	}).Methods("GET")
 
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
